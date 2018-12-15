aa4c934b7f7f1a7e146882a153e8a6c7d58e4931
Akynna <tim.nguyen@epfl.ch>
2018-12-15 14:18:11 +0100

    Improve the tests

--- tests/test_URB.py
+++ tests/test_URB.py
@@ -1,29 +1,38 @@
 import os
-import sys
 
 def main():
     
-    nb_proc = int(input("How many processes do you have?"))
+    print("Total list of processes:")
+    total_procs = [int(x) for x in input().split()]
     print("Enter the list of correct processes: ")
     corrects = [int(x) for x in input().split()]
-    print(corrects)
+    #print(corrects)
+    
+    nb_proc = len(total_procs)
     
     logs = [None] * nb_proc
     
+    # Initialize the list
     for i in range(nb_proc):
-        filename = '../da_proc_' + str(i+1) + '.out'
+        logs[i] = [None] * 501
+        
+    
+    for i in range(nb_proc):
+        filename = '../da_proc_' + str(total_procs[i]) + '.out'
         
         if os.stat(filename).st_size == 0:
-            print("File " + str(i+1) + " is empty.")
+            print("File " + str(total_procs[i]) + " is empty.")
             logs[i] = 0
         else:
             f = open(filename, 'r')
         
             if f.mode == 'r':
                 contents = f.read()
-                logs[i] = contents.split("\n")
+                logs[i][0] = total_procs[i] # Set the ID of process
+                logs[i][1:] = contents.split("\n")
                 
-                for m in range(len(logs[i])):
+                
+                for m in range(1, len(logs[i])):
                     logs[i][m] = logs[i][m].split(" ")
                     # print("Content of file " + str(i+1) + ":\n")
                     # print(logs[i][m])
@@ -36,11 +45,6 @@ def main():
     checkUniformAgreement(nb_proc, logs, corrects)      # URB4
     
 
-    
-    
-    # 4) Uniform Agreement: For any message m, if a process delivers m,
-    # then every correct process delivers m
-
 
 # 1) Validity: If pi and pj are correct, then every message broadcast by pi
 # is eventually delivered by pj
@@ -52,12 +56,13 @@ def checkValidity(nb_proc, logs, corrects):
     for i in range(nb_proc):
         
         current_proc = logs[i]
+        currentProcessID = current_proc[0]
         
         # If the process is correct, check the validity spec for all messages
-        if (i+1) in corrects:
+        if currentProcessID in corrects:
             
             # Iterate over all the messages of the current process
-            for mi in range(len(current_proc)):
+            for mi in range(1, len(current_proc)):
                 
                 # Get the next message which is a 'broadcast'
                 if current_proc[mi][0] == 'b':
@@ -65,34 +70,39 @@ def checkValidity(nb_proc, logs, corrects):
                     
                     # Compare with the other processes
                     for j in range(nb_proc):
-                        if j != i and logs[j] != 0:
-                            
-                            # Reset the boolean check
-                            isDelivered = False
-                            
-                            # Get the next process to be checked
-                            other_proc = logs[j]
-                            
-                            # Iterate over all 'delivered' messages
-                            # in the other process
-                            for mj in range(len(logs[j])):
+                        
+                        # Get the next process to be checked
+                        other_proc = logs[j]
+                        
+                        if other_proc != 0:
+                            otherProcessID = other_proc[0]
+                        
+                            if otherProcessID != currentProcessID:
                                 
-                                msg_to_check = other_proc[mj]
-                                
-                                if len(msg_to_check) != 0 and msg_to_check[0] == 'd':
+                                # Reset the boolean check
+                                isDelivered = False
+                        
+                                # Iterate over all 'delivered' messages
+                                # in the other process
+                                for mj in range(1, len(logs[j])):
                                     
-                                    # Check if the delivery corresponds to the
-                                    # broadcast
-                                    if int(msg_to_check[1])==(i+1) and int(msg_to_check[2])==int(current_msg[1]):
-                                        isDelivered = True
-                                        break
+                                    msg_to_check = other_proc[mj]
                                     
-                            if not isDelivered:
-                                print("Error of Validity: Process " + str(j+1)
-                                + " has failed to deliver message " + str(mi+1)
-                                + " from Process " + str(i+1))
-                                noError = False
-                                #sys.exit()
+                                    if len(msg_to_check) != 0 and msg_to_check[0] == 'd':
+                                        
+                                        # Check if the delivery corresponds to the
+                                        # broadcast
+                                        if int(msg_to_check[1])==currentProcessID and \
+                                        int(msg_to_check[2])==int(current_msg[1]):
+                                            isDelivered = True
+                                            break
+                                        
+                                if not isDelivered:
+                                    print("Error of Validity: Process " + otherProcessID
+                                    + " has failed to deliver message " + str(mi)
+                                    + " from Process " + str(i+1))
+                                    noError = False
+                                    #sys.exit()
     
     if noError:
         print("Validity specification is correct !")
@@ -113,12 +123,12 @@ def checkNoDuplication(nb_proc, logs):
             msg_list = [None] * len(current_proc)
         
             # Iterate over all the messages of the current process
-            for mi in range(len(current_proc)):
+            for mi in range(1, len(current_proc)):
                 msg = '-'.join(current_proc[mi])
                 
                 # Check if the message was duplicated
                 if(msg in msg_list):
-                    print("Error of duplication: Message " + str(mi+1)
+                    print("Error of duplication: Message " + str(mi)
                     + "has been duplicated")
                     break
                     noError = False
@@ -138,13 +148,14 @@ def checkNoCreation(nb_proc, logs):
     for i in range(nb_proc):
         
         current_proc = logs[i]
+        currentProcessID = current_proc[0]
         
         # If the list of messages is not empty,
         # check the no creation spec for all messages
         if current_proc != 0:
             
             # Iterate over all the messages of the current process
-            for mi in range(len(current_proc)):
+            for mi in range(1, len(current_proc)):
                 
                 # Get the next message which is a 'delivery'
                 if current_proc[mi][0] == 'd':
@@ -153,35 +164,38 @@ def checkNoCreation(nb_proc, logs):
                     # Compare with the other processes
                     for j in range(nb_proc):
                         
-                        # Find the process that is supposed to have broadcasted
-                        if j != i and logs[j] != 0 and int(current_msg[1]) == (j+1):
-                            
-                            # Reset the boolean check
-                            isBroadcast = False
-                            
-                            # Get the process to be checked
-                            other_proc = logs[j]
-                            
-                            # Iterate over all 'broadcast' messages
-                            # in this process
-                            for mj in range(len(logs[j])):
+                        # Get the process to be checked
+                        other_proc = logs[j]
+                        
+                        if(other_proc != 0):
+                            otherProcessID = other_proc[0]
+                        
+                            # Find the process that is supposed to have broadcasted
+                            if otherProcessID != currentProcessID and int(current_msg[1]) == otherProcessID:
                                 
-                                msg_to_check = other_proc[mj]
+                                # Reset the boolean check
+                                isBroadcast = False
                                 
-                                if len(msg_to_check) != 0 and msg_to_check[0] == 'b':
+                                # Iterate over all 'broadcast' messages
+                                # in this process
+                                for mj in range(1, len(logs[j])):
                                     
-                                    # Check if the delivery corresponds to the
-                                    # broadcast
-                                    if int(msg_to_check[1])==int(current_msg[2]):
-                                        isBroadcast = True
-                                        break
+                                    msg_to_check = other_proc[mj]
                                     
-                            if not isBroadcast:
-                                print("Error of Creation: Process " + str(j+1)
-                                + " has not broadcast message " + str(mi+1)
-                                + " to Process " + str(i+1))
-                                noError = False
-                                #sys.exit()
+                                    if len(msg_to_check) != 0 and msg_to_check[0] == 'b':
+                                        
+                                        # Check if the delivery corresponds to the
+                                        # broadcast
+                                        if int(msg_to_check[1])==int(current_msg[2]):
+                                            isBroadcast = True
+                                            break
+                                        
+                                if not isBroadcast:
+                                    print("Error of Creation: Process " + str(otherProcessID)
+                                    + " has not broadcast message " + str(mi)
+                                    + " to Process " + str(currentProcessID))
+                                    noError = False
+                                    #sys.exit()
     
     if noError:
         print("No Creation specification is correct !")
@@ -196,10 +210,11 @@ def checkUniformAgreement(nb_proc, logs, corrects):
     for i in range(nb_proc):
         
         current_proc = logs[i]
+        currentProcessID = current_proc[0]
         
         if current_proc != 0:
             # Iterate over all the messages of the current process
-            for mi in range(len(current_proc)):
+            for mi in range(1, len(current_proc)):
                 
                 # Get the next message which is a 'deliver'
                 if current_proc[mi][0] == 'd':
@@ -208,7 +223,12 @@ def checkUniformAgreement(nb_proc, logs, corrects):
                     # Compare with the other processes
                     for j in range(nb_proc):
                         
-                        if j != i and (j+1) in corrects and int(current_msg[1]) != (j+1):
+                        other_proc = logs[j]
+                        otherProcessID = other_proc[0]
+                        
+                        if otherProcessID != currentProcessID \
+                        and otherProcessID in corrects \
+                        and int(current_msg[1]) != otherProcessID:
                             
                             # Reset the boolean check
                             isDelivered = False
@@ -218,7 +238,7 @@ def checkUniformAgreement(nb_proc, logs, corrects):
                             
                             # Iterate over all 'delivered' messages
                             # in the other process
-                            for mj in range(len(logs[j])):
+                            for mj in range(1, len(logs[j])):
                                 
                                 msg_to_check = other_proc[mj]
                                 
@@ -233,7 +253,7 @@ def checkUniformAgreement(nb_proc, logs, corrects):
                                     
                             if not isDelivered:
                                 print("Error of Uniform Agreement: Process "
-                                      + str(j+1)
+                                      + str(otherProcessID)
                                       + " has failed to deliver message "
                                       + str(current_msg))
                                 noError = False
